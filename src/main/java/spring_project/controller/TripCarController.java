9b53eca1b679a95a4d0e731e41e51a78d3770547
duyduc1 <duc53044@donga.edu.vn>
2025-05-12 19:31:06 +0700

    api-tripcar

---
+++ src/main/java/spring_project/controller/TripCarController.java
@@ -0,0 +1,48 @@
+package spring_project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import spring_project.dto.TripCarRequets;
+import spring_project.entity.TripCar;
+import spring_project.service.TripCarService;
+
+import java.util.List;
+
+@RestController
+public class TripCarController {
+
+    @Autowired
+    private TripCarService tripCarService;
+
+    @GetMapping("/useradmin-all-tripcar")
+    public ResponseEntity<List<TripCarRequets>> getAllTripCar() {
+        List<TripCarRequets> listTripCars = tripCarService.getAllTripCars();
+        return new ResponseEntity<>(listTripCars, HttpStatus.OK);
+    }
+
+    @GetMapping("/useradmin-all-tripccar/{tripCarId}")
+    public ResponseEntity<TripCarRequets> getTripCarById(@PathVariable Long tripCarId) {
+        TripCarRequets tripCarById = tripCarService.getTripCarById(tripCarId);
+        return new ResponseEntity<>(tripCarById, HttpStatus.OK);
+    }
+
+    @PostMapping("/api-tripcar/create-tripcar")
+    public ResponseEntity<TripCarRequets> createTripCarByAdmin(@RequestBody TripCarRequets tripCarRequets) {
+        TripCar tripCar = tripCarService.createTripCar(tripCarRequets);
+        return new ResponseEntity<>(tripCarRequets, HttpStatus.OK);
+    }
+
+    @PutMapping("/api-tripcar/update-tripcar/{tripCarId}")
+    public ResponseEntity<TripCarRequets> updateTripCarByAdmin(@PathVariable Long tripCarId, @RequestBody TripCarRequets tripCarRequets) {
+        TripCar tripCar = tripCarService.updateTripCar(tripCarId, tripCarRequets);
+        return new ResponseEntity<>(tripCarRequets, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/api-tripcar/delete-tripcar/{tripCarId}")
+    public ResponseEntity<TripCarRequets> deleteTripCarByAdmin(@PathVariable Long tripCarId) {
+        tripCarService.deleteTripCar(tripCarId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- src/main/java/spring_project/service/TripCarService.java
+++ src/main/java/spring_project/service/TripCarService.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import spring_project.dto.TripCarRequets;
+import spring_project.entity.Coach;
+import spring_project.entity.Driver;
+import spring_project.entity.Rickshaw;
 import spring_project.entity.TripCar;
 import spring_project.mapper.TripCarMapper;
 import spring_project.repository.TripCarRepository;
@@ -41,5 +44,43 @@ public class TripCarService {
         return tripCarMapper.toDTO(tripCar);
     }
 
-    public
+    public TripCar createTripCar(TripCarRequets tripCarRequets) {
+        TripCar newTripCar = new TripCar();
+        newTripCar.setTripName(tripCarRequets.getTripName());
+        newTripCar.setEmptySeatNumber(tripCarRequets.getEmptySeatNumber());
+        newTripCar.setPriceSeatNumber(tripCarRequets.getPriceSeatNumber());
+        newTripCar.setSeatNumber(tripCarRequets.getSeatNumber());
+        newTripCar.setPayPonit(tripCarRequets.getPayPonit());
+        newTripCar.setPickupPoint(tripCarRequets.getPickupPoint());
+        newTripCar.setDepartureTime(tripCarRequets.getDepartureTime());
+        newTripCar.setDepartureEndTime(tripCarRequets.getDepartureEndTime());
+        newTripCar.setDepartureDate(tripCarRequets.getDepartureDate());
+        Driver driver = driverService.findDriverById(tripCarRequets.getDriverId());
+        Coach coach = coachSevice.findCoachById(tripCarRequets.getCoachId());
+        Rickshaw rickshaw = rickShawService.getRickShaw(tripCarRequets.getRickshawId());
+        newTripCar.setDriver(driver);
+        newTripCar.setCoach(coach);
+        newTripCar.setRickshaw(rickshaw);
+        return tripCarRepository.save(newTripCar);
+    }
+
+    public TripCar updateTripCar(Long tripCarId, TripCarRequets tripCarRequets) {
+        TripCar tripCar = tripCarRepository.findById(tripCarId).orElse(null);
+        tripCar.setTripName(tripCarRequets.getTripName());
+        tripCar.setEmptySeatNumber(tripCarRequets.getEmptySeatNumber());
+        tripCar.setPriceSeatNumber(tripCarRequets.getPriceSeatNumber());
+        tripCar.setSeatNumber(tripCarRequets.getSeatNumber());
+        tripCar.setPayPonit(tripCarRequets.getPayPonit());
+        tripCar.setPickupPoint(tripCarRequets.getPickupPoint());
+        tripCar.setDepartureTime(tripCarRequets.getDepartureTime());
+        tripCar.setDepartureEndTime(tripCarRequets.getDepartureEndTime());
+        tripCar.setDepartureDate(tripCarRequets.getDepartureDate());
+        return tripCarRepository.save(tripCar);
+    }
+
+    public boolean deleteTripCar(Long tripCarId) {
+        TripCar tripCar = tripCarRepository.findById(tripCarId).orElse(null);
+        tripCarRepository.delete(tripCar);
+        return true;
+    }
 }
